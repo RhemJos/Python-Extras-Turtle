475202478b7f46a0d189d8e8d30429b4c7808fc9
RhemJos <rhemjos@gmail.com>
2026-06-18 13:40:46 -0400

    new advance

--- AA/04 Draw square fill color speed hide.py
+++ AA/04 Draw square fill color speed hide.py
@@ -5,7 +5,13 @@ window.bgcolor("black")
 window.title("Animation by Rhem")
 
 t = turtle.Turtle()
+t.pensize(10)
+t.speed(0)  # fastest 0, fast 10, normal 6, slow 3, slowest 1
+t.hideturtle()
 t.color('green')
+
+t.begin_fill()
+t.fillcolor('light blue')
 t.fd(100)
 t.lt(90)
 t.fd(100)
@@ -15,5 +21,6 @@ t.lt(90)
 t.fd(100)
 t.lt(90)
 
+t.end_fill()
 
 turtle.exitonclick()
